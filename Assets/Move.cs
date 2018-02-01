05da67ba0830d05569c81dee9f392219c6e6c589
WoongPyo Hong <hongup1@naver.com>
2018-02-01 17:10:56 +0900

    move 추가2

---
+++ Assets/Move.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+	void Start () {
+        Debug.Log("Start() called.");
+	}
+	void Update () {
+        var dx = Input.GetAxis("Horizontal");
+        var dz = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(dx, 0, dz));
+	}
+    private void OnTriggerEnter(Collider other) {
+        Debug.LogFormat("OnTroggerEnter called. other is {0}", other.name);
+    }
+    private void OnTriggerExit(Collider other) {
+        Debug.LogFormat("OnTroggerExit called. other is {0}", other.name);
+    }
+}
