75c30304e8a2eb33265bc64d6db2d54b291dcc76
WoongPyo Hong <hongup1@naver.com>
2018-02-02 16:53:17 +0900

    pad script add

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -1,14 +1,23 @@
 ﻿using UnityEngine;
 
 public class Move : MonoBehaviour {
-	void Start () {
+    public float speed = 5.0f;
+    public DirPad dirPad;
+
+    void Start () {
         Debug.Log("Start() called.");
 	}
 	void Update () {
-        var dx = Input.GetAxis("Horizontal");
-        var dz = Input.GetAxis("Vertical");
-
-        transform.Translate(new Vector3(dx, 0, dz));
+        //Debug.LogFormat("Time.deltaTime = {0}", Time.deltaTime);
+        if (dirPad.dragging) {
+            var dn = dirPad.dir.normalized * Time.deltaTime * speed;
+            transform.Translate(new Vector3(dn.x, 0, dn.y));
+        }
+        else {
+            var dx = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+            var dz = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+            transform.Translate(new Vector3(dx, 0, dz));
+        }
 	}
     private void OnTriggerEnter(Collider other) {
         Debug.LogFormat("OnTroggerEnter called. other is {0}", other.name);
---
+++ Assets/TextChange.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TextChange : MonoBehaviour {
+    Text testText;
+	void Start () {
+        testText = GetComponent<Text>();
+	}
+	void Update () {
+        testText.text = string.Format("프레임 수 :{0}", 1/Time.deltaTime);
+	}
+}
